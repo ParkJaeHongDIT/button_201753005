28b35049c24b5f212de88258619f3e6bbccb5199
dit02 <dit@imac-7.local>
2019-03-12 15:30:22 +0900

    second commit

--- button_201753005/ViewController.swift
+++ button_201753005/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+      //  print("Hello Button Event!")
+        myLabel.text = "Hello Button Event!"
+    }
+    
+    @IBAction func Init(_ sender: Any) {
+        myLabel.text = ""
+    }
+    @IBAction func changeViewColor(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    
 }
 
